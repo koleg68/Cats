059c2cd2a7c8e011eb52e9fa1d644d1f6434a738
koleg68 <9326538@gmail.com>
2019-03-02 12:04:31 +0300

    плавный скролл к якорям

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+// собираем все якоря; устанавливаем время анимации и количество кадров
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+    animationTime = 300,
+    framesCount = 20;
+
+anchors.forEach(function (item) {
+    // каждому якорю присваиваем обработчик события
+    item.addEventListener('click', function (e) {
+        // убираем стандартное поведение
+        e.preventDefault();
+
+        // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+        var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
+
+        // запускаем интервал, в котором
+        var scroller = setInterval(function () {
+            // считаем на сколько скроллить за 1 такт
+            var scrollBy = coordY / framesCount;
+
+            // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+            // и дно страницы не достигнуто
+            if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+                // то скроллим на к-во пикселей, которое соответствует одному такту
+                window.scrollBy(0, scrollBy);
+            } else {
+                // иначе добираемся до элемента и выходим из интервала
+                window.scrollTo(0, coordY);
+                clearInterval(scroller);
+            }
+            // время интервала равняется частному от времени анимации и к-ва кадров
+        }, animationTime / framesCount);
+    });
+});
+  
   var Data = new Date();
   //   console.log(Data.getDate());
   var Month = Data.getMonth();
